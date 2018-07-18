89727573971f6c162667bb0e34f8b64ce2e9c067
Alex Ruiz <alex.ruiz@ofertia.com>
2018-07-18 19:18:50 +0200

    Create Structure with Docker for Redis+RubyApp
    Create Basic dockerfiles for redis / rubyapp
    without any dependency between them in order
    to start dancing.

---
+++ projects/ruby-app/hello-world.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'redis'
+
+# redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
+
+get '/' do
+  "Hello World!"
+end

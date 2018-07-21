938c648f173b009850fbf118cdec23f68a840a9f
Alex Ruiz Estradera <alex.ruiz@ofertia.com>
2018-07-22 01:01:20 +0200

    Allow REDIS_PASSWORD in ruby app

--- projects/ruby-app/hello-world.rb
+++ projects/ruby-app/hello-world.rb
@@ -3,7 +3,7 @@
 require 'sinatra'
 require 'redis'
 
-redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
+redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379, :port => ENV["REDIS_PASSWORD"] || nil)
 
 get '/' do
   redis.ping

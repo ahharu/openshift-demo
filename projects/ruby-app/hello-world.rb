9b7e779d14565663de388d3914b56ffce5da7c75
Alex Ruiz Estradera <alex.ruiz@ofertia.com>
2018-07-22 00:22:34 +0200

    Add RubyApp Redis Ping

--- projects/ruby-app/hello-world.rb
+++ projects/ruby-app/hello-world.rb
@@ -3,8 +3,9 @@
 require 'sinatra'
 require 'redis'
 
-# redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
+redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
 
 get '/' do
+  redis.ping
   "Hello World!"
 end

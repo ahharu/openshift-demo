fed10fcb6c6dbbb5beadedfb9c9729402fbad9c1
Alex Ruiz Estradera <alex.ruiz@ofertia.com>
2018-07-22 02:08:31 +0200

    Fix typo in hello-world.rb password

--- projects/ruby-app/hello-world.rb
+++ projects/ruby-app/hello-world.rb
@@ -3,7 +3,7 @@
 require 'sinatra'
 require 'redis'
 
-redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379, :port => ENV["REDIS_PASSWORD"] || nil)
+redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379, :password => ENV["REDIS_PASSWORD"] || nil)
 
 get '/' do
   redis.ping
